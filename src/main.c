f806dff25db1d7fc1dfcf5fcf1a20d0e5cbf295d
ech0x0 <141404426+ech0x0@users.noreply.github.com>
2023-10-11 18:21:52 +0300

    finished sha256 and sha512

--- src/main.c
+++ src/main.c
@@ -1,19 +1,15 @@
 #include "hash/sha256.h"
+#include "hash/sha512.h"
 #include <stdio.h>
 #include <time.h>
 #include <inttypes.h>
 
 int main(int argc, char** argv) {
-    double time = 0;
-    for (int j = 0; j < 100; ++j) {
-        clock_t b = clock();
-        for (int i = 0; i < 10000; ++i) {
-            uint8_t message[4] = { 0x61, 0x62, 0x63, (uint8_t)(i % 255) };
-            uint32_t hash[8];
-            sha256(hash, message, 4 * 8);
-        }
-        clock_t e = clock();
-        time += (double)(e - b) / CLOCKS_PER_SEC;
+    uint32_t hash[8];
+    uint8_t msg[3] = { 'a', 'b', 'c' };
+    sha256(hash, msg, 3 * 8);
+    for (int i = 0; i < 8; ++i) {
+        printf("%x", hash[i]);
     }
-    printf("%f\n", time);
+    printf("\n");
 }
